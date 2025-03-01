16cde2fe956599b165893541c8d520cf2bf8aa2b
unawarespecs <christiandegala@hotmail.com>
2025-03-01 09:06:25 +0800

    new index page, content not added yet

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+
+<html>
+
+<head>
+    <title>Test</title>
+</head>
+
+<body>
+    <h1>Hello world!</br></h1>
+    <a href="index.php">Home Page</a>
+    <a href="pages/aboutus.php">About Us</a>
+    <a href="pages/announcements.php">Announcements</a>
+    <a href="pages/donate.php">Donate</a>
+    <a href="pages/schedule.php">Schedule an Outreach</a>
+</body>
+
+</html>
