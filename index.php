ac8707735008c22235e3b099f250dcb768b28a06
unawarespecs <christiandegala@hotmail.com>
2025-03-01 09:06:52 +0800

    more updates

--- index.php
+++ index.php
@@ -1,20 +1,83 @@
 <?php
-
 ?>
 
-<html>
+<html lang="en">
 
 <head>
-    <title>Test</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.88.1">
+    <title>Tahanan Outreach Projects & Services, Inc</title>
+
+    <!-- <link rel="icon" type="image/x-icon" href=""> -->
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/cover/">
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+        .bd-placeholder-img {
+            background-image: url('assets/images/bg-masthead.jpg');
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-position: center;
+        }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+    </style>
+
+    <!-- Custom styles for this template -->
+    <link href="css/homepage-cover.css" rel="stylesheet">
+    <link href="css/homepage.css" rel="stylesheet">
 </head>
 
-<body>
-    <h1>Hello world!</br></h1>
-    <a href="index.php">Home Page</a>
-    <a href="pages/aboutus.php">About Us</a>
-    <a href="pages/announcements.php">Announcements</a>
-    <a href="pages/donate.php">Donate</a>
-    <a href="pages/schedule.php">Schedule an Outreach</a>
+<body class="d-flex h-100 text-center text-white bd-placeholder-img bg-transparent">
+
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+        <header class="mb-auto">
+            <div>
+                <img src="images/topslogo.png" class="TOPSlogo" alt="TOPS logo">
+                <h3 class="float-md-start mb-0">Tahanan Outreach Projects & Services, Inc</h3>
+                <nav class="nav nav-masthead justify-content-center float-md-end">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link" href="pages/aboutus.php">About Us</a>
+                    <a class="nav-link" href="pages/announcements.php">Announcements</a>
+                    <a class="nav-link" href="pages/schedule.php">Outreach Schedule</a>
+                    <a class="nav-link" href="pages/donate.php">Donate</a>
+                    <a class="nav-link" href="pages/contact.php">Contact Us</a>
+                    <a class="nav-link" href="pages/login.php">Login/Register</a>
+                </nav>
+            </div>
+        </header>
+
+        <main class="px-3">
+            <h1><span style="color: #00704A">[Placeholder]</span></h1>
+            <p class="lead"> <?php
+            echo "Hi, I'm a PHP script!";
+            ?>
+            </p>
+
+            <p class="lead">
+                <a href="pages/aboutus.php" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Learn
+                    more</a>
+            </p>
+        </main>
+
+        <footer class="mt-auto text-white-50">
+            <p>Copyright &copy; 2025, <a class="text-white"
+                    href="https://github.com/unawarespecs/it-project/blob/main/GROUP_MEMBERS.md">Mapua Webdevs</a> and
+                <a class="text-white" href="https://example.com">Tahanan Outreach Projects & Services, Inc</a>. We do
+                not own the TOPS brand.
+            </p>
+        </footer>
+    </div>
+
 </body>
 
 </html>
